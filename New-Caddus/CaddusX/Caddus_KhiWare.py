b02d7cf72395da62ff1f4395a90d15b1e1116054
RobbScript <96984211+RobbScript@users.noreply.github.com>
2022-11-02 14:31:27 -0400

    Update Caddus_KhiWare.py

--- New-Caddus/CaddusX/Caddus_KhiWare.py
+++ New-Caddus/CaddusX/Caddus_KhiWare.py
@@ -4,7 +4,7 @@ import os
 import json
 if os.path.isfile('UserPass.json'):
    os.system("clear")
-   print("CaddusX (KhiWare) | Versão 2.0.0")
+   print("CaddusX (KhiWare) | Versão 2.0.1")
    NewUserJson = open('UserPass.json')
    data = json.load(NewUserJson)
    User = (data["user"])
